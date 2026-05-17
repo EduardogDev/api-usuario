eea63934b4c58094f04b1624ae85a2f8271a5715
EduardogDev <eduardosgal14@gmail.com>
2026-05-17 20:32:07 -0300

    feat: cria a entidade usuario

---
+++ src/main/java/com/eduardodev/api_usuario/infrastructure/entities/usuario.java
@@ -0,0 +1,47 @@
+package com.eduardodev.api_usuario.infrastructure.entities;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document(collection = "usuario")
+public class usuario {
+
+    @Id
+    private String ID;
+    private String nome;
+    private String email;
+
+
+    public String getID() {
+        return ID;
+    }
+
+    public void setID(String ID) {
+        this.ID = ID;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public usuario() {
+    }
+
+    public usuario(String ID, String nome, String email) {
+        this.ID = ID;
+        this.nome = nome;
+        this.email = email;
+    }
+}
