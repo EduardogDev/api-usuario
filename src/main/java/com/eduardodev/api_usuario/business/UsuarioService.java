f11123e519b2cf4830d54bb8e02c0de92d25b461
EduardogDev <eduardosgal14@gmail.com>
2026-05-18 21:18:26 -0300

    feat: implementação completa do CRUD de usuários

--- src/main/java/com/eduardodev/api_usuario/business/UsuarioService.java
+++ src/main/java/com/eduardodev/api_usuario/business/UsuarioService.java
@@ -2,7 +2,12 @@ package com.eduardodev.api_usuario.business;
 
 import com.eduardodev.api_usuario.infrastructure.entities.Usuario;
 import com.eduardodev.api_usuario.infrastructure.repository.UsuarioRepository;
+import org.jspecify.annotations.Nullable;
+import org.springframework.data.annotation.Id;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
 
 @Service
 public class UsuarioService {
@@ -16,5 +21,21 @@ public class UsuarioService {
 
     public Usuario salvarUsuario(Usuario usuario) {
         return usuarioRepository.save(usuario);
+    }
+
+    public List<Usuario> buscarUsuario() {
+        return usuarioRepository.findAll();
+    }
+
+    public Usuario atualizarUsuario(Usuario usuario){
+        Optional<Usuario> usuarioExiste = usuarioRepository.findById(usuario.getID());
+
+        if(usuarioExiste.isPresent()){
+            return usuarioRepository.save(usuario);
+        }return new Usuario();
+    }
+
+    public void deletarUsuario(String Id){
+        usuarioRepository.deleteById(Id);
     }
 }
--- src/main/java/com/eduardodev/api_usuario/controller/UsuarioController.java
+++ src/main/java/com/eduardodev/api_usuario/controller/UsuarioController.java
@@ -3,10 +3,9 @@ package com.eduardodev.api_usuario.controller;
 import com.eduardodev.api_usuario.business.UsuarioService;
 import com.eduardodev.api_usuario.infrastructure.entities.Usuario;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/usuarios")
@@ -21,5 +20,21 @@ public class UsuarioController {
     @PostMapping
     public ResponseEntity<Usuario> salvarUsuario(@RequestBody Usuario usuario) {
         return ResponseEntity.ok().body(usuarioService.salvarUsuario(usuario));
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Usuario>> buscarUsuarios(){
+        return ResponseEntity.ok().body(usuarioService.buscarUsuario());
+    }
+
+    @PutMapping
+    public ResponseEntity<Usuario> atualizarUsuario(@RequestBody Usuario usuario) {
+        return ResponseEntity.ok().body(usuarioService.atualizarUsuario(usuario));
+    }
+
+    @DeleteMapping
+    public ResponseEntity<Void> deletarUsuario(@RequestParam("id")String Id){
+        usuarioService.deletarUsuario(Id);
+        return ResponseEntity.accepted().build();
     }
 }
